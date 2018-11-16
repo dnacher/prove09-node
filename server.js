ab22d96454eeaad1166509c88f49a94778782e9c
DesDanielNacher <daniel.nacher@despegar.com>
2018-11-16 14:49:36 -0300

    v1.0.2

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (req, res) => res.send('<h1>Hello World</h1>'))
+app.get('/index', (req, res) => res.send('<h1>Hello World</h1>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
