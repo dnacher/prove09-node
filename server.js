5f868854f2c46f003ba6f0e9e4d221566f85361e
DesDanielNacher <daniel.nacher@despegar.com>
2018-11-16 15:01:23 -0300

    v1.0.4

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views/pages');
+app.set('view engine', 'ejs');
+
 var port = app.get('port');
 
 app.get('/index', (req, res) => res.send('<h1>Hello World</h1>'));
