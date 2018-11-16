c6494c406248355b2a6c8d1150a652cfe43d4ecf
DesDanielNacher <daniel.nacher@despegar.com>
2018-11-16 14:47:14 -0300

    v1.0.1

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const url = require('url');
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', (req, res) => res.send('<h1>Hello World</h1>'))
 
